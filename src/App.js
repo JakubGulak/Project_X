c2cf26dad1469b7c4bbd6538773095104415e1fb
uczen <uczem@ckziu.net>
2023-09-07 09:19:15 +0200

    add firebase db and its done for today

--- src/App.js
+++ src/App.js
@@ -1,4 +1,20 @@
-import './App.css';
+import React from 'react';
+import './App.css'; 
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCmEpLni3odJKcN9CrTqKaKxIu_tu4LBR8",
+  authDomain: "libraryckziu.firebaseapp.com",
+  projectId: "libraryckziu",
+  storageBucket: "libraryckziu.appspot.com",
+  messagingSenderId: "109161048917",
+  appId: "1:109161048917:web:7be4751f529e97175a1ddc",
+  measurementId: "G-EZLM6V2WPN"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App() {
   return (
@@ -9,3 +25,4 @@ function App() {
 }
 
 export default App;
+
