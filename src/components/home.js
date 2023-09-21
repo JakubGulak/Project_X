657341a98416ca33a7974ddc7a6f8bb9819f690c
uczen <uczem@ckziu.net>
2023-09-21 09:08:22 +0200

    working with icons

--- src/components/home.js
+++ src/components/home.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import facebook from './facebook.png';
+import instagram from './instagram.png';
 
 function Home() {
   return (
     <div>
       <div id="header">
-          <h1>Biblioteka Szkolna CKZiU w Jaworznie</h1>
-
-      </div>
-      <a href="https://www.facebook.com/twojaprofil" target="_blank" rel="noopener noreferrer">
-          <FontAwesomeIcon icon={faFacebook} />
+        <div id="header-content">
+          <a href="https://www.facebook.com/twojaprofil" target="_blank" rel="noopener noreferrer">
+            <img src={facebook} alt="Facebook" id='firsticon' />
           </a>
-        <a href="/main">Login</a>
+          <a href="https://www.facebook.com/twojaprofil" target="_blank" rel="noopener noreferrer">
+            <img src={instagram} alt="Instagram" id='secondicon' />
+          </a>
+          <h1>Biblioteka Szkolna CKZiU w Jaworznie</h1>
+          <a href="/main">Login</a>
+        </div>
+      </div>
     </div>
   );
 }
