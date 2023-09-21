282dc9f918af98f4642ed141651229846ada76d8
uczen <uczem@ckziu.net>
2023-09-21 08:22:23 +0200

    add image and icons

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 
 function Home() {
   return (
     <div>
       <div id="header">
           <h1>Biblioteka Szkolna CKZiU w Jaworznie</h1>
+
       </div>
+      <a href="https://www.facebook.com/twojaprofil" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faFacebook} />
+          </a>
         <a href="/main">Login</a>
     </div>
   );
