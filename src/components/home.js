525db6b5a25366466549ecc3a9c2c6fee9bc292d
uczen <uczem@ckziu.net>
2023-10-25 11:12:39 +0200

    small verison of session, very good job today

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 import facebook from './facebook.png';
 import instagram from './instagram.png';
-import ckziu from './ckziu.png'
-import './home.css'
+import ckziu from './ckziu.png';
+import './home.css';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD9cGsHCJl5ciSZlMxLdW-oVq5w9hfc1MM",
+  authDomain: "userslibraryckziu.firebaseapp.com",
+  projectId: "userslibraryckziu",
+  storageBucket: "userslibraryckziu.appspot.com",
+  messagingSenderId: "528770481702",
+  appId: "1:528770481702:web:1242274cbcfb9cdf9bb0e7",
+  measurementId: "G-C543MMB5Z0"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 function Home() {
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Sprawdź stan uwierzytelnienia użytkownika po załadowaniu strony
+    const unsubscribe = firebase.auth().onAuthStateChanged((authUser) => {
+      if (authUser) {
+        // Użytkownik jest zalogowany
+        setUser(authUser);
+      } else {
+        // Brak użytkownika zalogowanego
+        setUser(null);
+      }
+    });
+
+    return () => {
+      // Odsubskrybuj zdarzenia, aby uniknąć wycieków pamięci
+      unsubscribe();
+    }
+  }, []);
+
+  const handleLogout = async () => {
+    try {
+      // Wyloguj użytkownika i zakończ sesję
+      await firebase.auth().signOut();
+      navigate('/'); // Przenieś użytkownika na stronę główną po wylogowaniu
+    } catch (error) {
+      console.error("Błąd wylogowania:", error);
+    }
+  };
+
   return (
     <div id='app'>
       <div id="header">
@@ -21,7 +70,14 @@ function Home() {
               </a>
           </div>
           <h1 id='title'>Biblioteka Szkolna CKZiU w Jaworznie</h1>
-          <a href="/login">Zaloguj się!</a>
+          {user ? (
+            <div>
+              <p id='hello'>Witaj, {user.email}!</p>
+              <button onClick={handleLogout}>Wyloguj się</button>
+            </div>
+          ) : (
+            <a href="/login">Zaloguj się!</a>
+          )}
         </div>
       </div>
       <div id='content'></div>
